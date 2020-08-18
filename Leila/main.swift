46958f1a313d3d3be0ca7aef66f6117eecf370fa
Erick Almeida <erickingles@gmail.com>
2020-08-18 12:31:31 -0300

    Initial Commit

---
+++ Leila/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Leila
+//
+//  Created by Erick Almeida on 18/08/20.
+//  Copyright © 2020 Erick Almeida. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
